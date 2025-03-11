1937342a712eb1d7c2b842c53d92dbfef5fbbcad
Somnusochi <somnusochi@gmail.com>
2025-03-11 09:47:47 +0800

    feat: 删除部分无用代码

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,8 +3,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
-import FlipWords from '@/components/FlipWords.vue'
-import MarqueeWords from '@/components/MarqueeWords.vue'
+
 
 export default {
   extends: DefaultTheme,
@@ -13,8 +12,5 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ app, router, siteData }) {
-    app.component('FlipWords', FlipWords)
-    app.component('MarqueeWords', MarqueeWords)
-  }
+  enhanceApp({ app, router, siteData }) {}
 } satisfies Theme
