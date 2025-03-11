08a0621c9f10617be02aa15034a85723413f8b7a
Somnusochi <somnusochi@gmail.com>
2025-03-11 23:18:20 +0800

    feat: 优化组件

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,6 +3,8 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import FlipWords from '@/components/FlipWords.vue'
+import MarqueeWords from '@/components/MarqueeWords.vue'
 
 
 export default {
@@ -12,5 +14,9 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
+  enhanceApp({ app, router, siteData }) {
+    app.component('FlipWords', FlipWords)
+    app.component('MarqueeWords', MarqueeWords)
+  }
   enhanceApp({ app, router, siteData }) {}
 } satisfies Theme
